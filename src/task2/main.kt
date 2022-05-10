f94d0e38da415b20a39c583ddf45fd4135e7531b
SavitskyRoman <savitskyroman508@gmail.com>
2022-05-10 11:02:49 +0300

    added task2

--- src/task2/main.kt
+++ src/task2/main.kt
@@ -1,2 +1,16 @@
 package task2
+fun main() {
+    val two = getGen()
+    println(two)
+}
+
+data class Gen(val x: Int, val y: Int)
+const val SIZE_ALL = 10
+fun getGen(): List<Gen> {
+    val z = arrayListOf<Gen>()
+    for (i in 0..SIZE_ALL){
+        z.add(Gen(kotlin.random.Random.nextInt(SIZE_ALL), kotlin.random.Random.nextInt(SIZE_ALL)))
+    }
+    return z
+}
 
