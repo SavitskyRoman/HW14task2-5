e32eca799d948f61b81a4b99642a9ac5e190d487
SavitskyRoman <savitskyroman508@gmail.com>
2022-05-12 09:49:27 +0300

    added task4

--- src/task4/main.kt
+++ src/task4/main.kt
@@ -1,2 +1,15 @@
 package task4
 
+abstract class Owner {
+    abstract val a: Int
+    abstract val b: Int
+}
+
+class Util(override val a: Int, override val b: Int) : Owner() {
+    fun sum() = a + b
+}
+
+fun main() {
+    val x = Util(3, 5)
+    println(x.sum())
+}
